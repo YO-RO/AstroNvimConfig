028bf872ee8004f82e17c8417380d7db8a1a703e
YO-RO <45329703+YO-RO@users.noreply.github.com>
2023-11-08 21:03:17 +0900

    refactor: 不要な設定を削除

--- init.lua
+++ init.lua
@@ -7,24 +7,4 @@ return {
       wrap = true,
     },
   },
-
-  -- PyrightをRyeに対応させる
-  lsp = {
-    servers = {
-      "pyright",
-    },
-    config = {
-      pyright = {
-        settings = {
-          python = {
-            venvPath = ".",
-            pythonPath = "./.venv/bin/python",
-            analysis = {
-              extraPaths = { "." },
-            },
-          },
-        },
-      },
-    },
-  },
 }
